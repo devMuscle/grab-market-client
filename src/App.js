11c0d7773265d05d8fb8909aa1e720d4fea31619
devMuscle <hongsbien@naver.com>
2021-12-21 17:52:02 +0900

    Link 태그 적용

--- src/App.js
+++ src/App.js
@@ -1,9 +1,26 @@
 import logo from "./logo.svg";
 import "./App.css";
 import MainpageComponet from "./main";
+import { Switch, Route } from "react-router-dom";
+import UploadPage from "./upload";
+import ProductPage from "./product";
 
 function App() {
-  return <MainpageComponet />;
+  return (
+    <div>
+      <Switch>
+        <Route exact={true} path={"/"}>
+          <MainpageComponet />
+        </Route>
+        <Route exact={true} path="/products/:id">
+          <ProductPage />
+        </Route>
+        <Route exact={true} path="/upload">
+          <UploadPage />
+        </Route>
+      </Switch>
+    </div>
+  );
 }
 
 export default App;
